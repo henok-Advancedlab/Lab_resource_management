fa65e606b477c092557ff29026d61154ec2bf30e
Natnael-Fikire <fikirenatnael@gmail.com>
2026-05-30 16:01:08 +0300

    Added private methods for setting maintenance record and JSON serialization

--- app/controllers/maintenance_records_controller.rb
+++ app/controllers/maintenance_records_controller.rb
@@ -13,4 +13,20 @@ class MaintenanceRecordsController < ApplicationController
 
   def destroy
   end
+
+    private
+
+  def set_maintenance_record
+    @maintenance_record = MaintenanceRecord.includes(:equipment).find(params[:id])
+  end
+
+  def maintenance_record_params
+    params.require(:maintenance_record).permit(:equipment_id, :performed_at, :description)
+  end
+
+  def maintenance_record_json(record)
+    record.as_json(only: %i[id equipment_id performed_at description created_at updated_at]).merge(
+      equipment_name: record.equipment&.name
+    )
+  end
 end
